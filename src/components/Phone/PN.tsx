d76f15114dc0dd8d22982a6740678318d88e2fa7
hi-radik <practice.radomir@gmail.com>
2023-02-07 00:10:38 +0300

    priva

--- src/components/Phone/PN.tsx
+++ src/components/Phone/PN.tsx
@@ -1,9 +1,7 @@
 import PhoneInput from 'react-phone-input-2'
 import 'react-phone-input-2/lib/material.css'
 import { useState } from "react";
-export default function PN() {
-  // `value` will be the parsed phone number in E.164 format.
-  // Example: "+12133734253".
+export function PN() {
   const [value, setValue]:any = useState();
   return (
     <PhoneInput
