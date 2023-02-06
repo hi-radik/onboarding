493c3437dfbc8e3a4c122982deae012157b1917e
hi-radik <practice.radomir@gmail.com>
2023-02-06 23:29:15 +0300

    priva

--- src/routes/Phone.tsx
+++ src/routes/Phone.tsx
@@ -1,13 +1,11 @@
-import React from 'react'
-import PhoneInput from 'react-phone-input-2'
-import 'react-phone-input-2/lib/style.css'
+import React from "react";
+import PhoneInput from "react-phone-input-2";
+import "react-phone-input-2/lib/style.css";
 
+const PN = () => {
+  return <PhoneInput country={"us"} />;
+};
 
 export default function Phone() {
-  return (
-    <div><PhoneInput
-    country={'us'}
-    
-  /></div>
-  )
+  return <div><PN/></div>;
 }
