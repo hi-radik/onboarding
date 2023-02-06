65a0d106a64d415f4bf081d3222712b801cff8b1
hi-radik <practice.radomir@gmail.com>
2023-02-07 00:25:54 +0300

    gooooo

--- src/components/Phone/PN.tsx
+++ src/components/Phone/PN.tsx
@@ -1,7 +1,7 @@
 import PhoneInput from 'react-phone-input-2'
 import 'react-phone-input-2/lib/material.css'
 import { useState } from "react";
-export function PN() {
+export default function PN() {
   const [value, setValue]:any = useState();
   return (
     <PhoneInput
