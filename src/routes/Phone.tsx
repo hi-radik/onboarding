f5a174eaf4ef158416e8836321db3cf0c1f8996d
hi-radik <practice.radomir@gmail.com>
2023-02-06 22:25:04 +0300

    priva

--- src/routes/Phone.tsx
+++ src/routes/Phone.tsx
@@ -3,15 +3,26 @@ import PhoneInput from "react-phone-input-2";
 import "react-phone-input-2/lib/style.css";
 import { useState } from "react";
 
-export function Phone() {
-  const [state, setState] = useState('');
+
+export default function Phone() {
+  const [state, setState] = useState("");
+  const Ph = (
+    <PhoneInput
+      country={"ru"}
+      value={state}
+      onChange={(state) => setState(state)}
+    />
+  );
   return (
-    <div style={{display:'flex', justifyContent:'center', alignItems:'center',height:'100vh'}}>
-      <PhoneInput
-        country={"ru"}
-        value={state}
-        onChange={(state) => setState(state)}
-      />
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}
+    >
+      {Ph}
       {/* <input type="text" /> */}
     </div>
   );
