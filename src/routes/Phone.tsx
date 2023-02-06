538e23c7ac148d8558c90f3e901e7c84d3e2bb3f
hi-radik <practice.radomir@gmail.com>
2023-02-06 23:13:58 +0300

    priva

--- src/routes/Phone.tsx
+++ src/routes/Phone.tsx
@@ -2,32 +2,25 @@ import "react-phone-input-2/lib/style.css";
 import PhoneInput from "react-phone-input-2";
 import React from "react";
 import { useState } from "react";
-import { Component } from "react";
-type MyState = {
-  phone: string; // like this
-};
-class Phone extends Component {
-  state: MyState = {
-    phone: '',
-  };
-  render() {
-    return (
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          height: "100vh",
-        }}
-      >
-        <PhoneInput
-          country={"us"}
-          value={this.state.phone}
-          onChange={(phone) => this.setState(phone)}
-        />
-      </div>
-    );
-  }
-}
 
-export default Phone;
+export default function Phone() {
+  const [state, setState] = useState("");
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}
+    >
+      <h1>Priva</h1>
+      {/* <PhoneInput
+        country={"ru"}
+        value={state}
+        onChange={(state) => setState(state)}
+      /> */}
+    </div>
+  );
+}
