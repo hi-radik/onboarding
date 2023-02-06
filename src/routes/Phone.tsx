6d9500ebfb0f6737aad14a5ee836efaffa889843
hi-radik <practice.radomir@gmail.com>
2023-02-06 22:13:50 +0300

    priva

--- src/routes/Phone.tsx
+++ src/routes/Phone.tsx
@@ -6,13 +6,13 @@ import { useState } from "react";
 export default function Phone() {
   const [state, setState] = useState('');
   return (
-    <div style={{display:'flex', justifyContent:'center', alignItems:'center'}}>
-      {/* <PhoneInput
+    <div style={{display:'flex', justifyContent:'center', alignItems:'center',height:'100vh'}}>
+      <PhoneInput
         country={"ru"}
         value={state}
         onChange={(state) => setState(state)}
-      /> */}
-      <input type="text" />
+      />
+      {/* <input type="text" /> */}
     </div>
   );
 }
