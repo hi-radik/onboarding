a3d63fb5b00a81599981ccc105eaa3062b08ee04
hi-radik <practice.radomir@gmail.com>
2023-02-08 00:43:22 +0300

    ??

--- src/components/Phone/PN.tsx
+++ src/components/Phone/PN.tsx
@@ -1,25 +1,44 @@
-import 'react-phone-input-2/lib/bootstrap.css'
+import "react-phone-input-2/lib/bootstrap.css";
 import RPI from "react-phone-input-2";
+import "./PN.css";
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 const ReactPhoneInput = RPI.default ? RPI.default : RPI;
 
 import { useState } from "react";
 export default function PN() {
-  const [value, setValue]:any = useState();
+  const [value, setValue]: any = useState();
   return (
-    <ReactPhoneInput
-      placeholder="Номер телефона"
-      value={value}
-      onChange={setValue}
-      country={'ru'}
-      inputProps={{
-        name: 'phone',
-        required: true,
-        autoFocus: true
-      }}
-      disableDropdown
-      
-    />
+    <div className="phone-page">
+      <div style={{ width: "100%", display: "flex", justifyContent: "center" }}>
+        <img src="src/assets/logo.svg" alt="Лого" />
+      </div>
+      <ReactPhoneInput
+        placeholder="Номер телефона"
+        value={value}
+        onChange={setValue}
+        country={"ru"}
+        inputProps={{
+          name: "phone",
+          required: true,
+          autoFocus: true,
+        }}
+        disableDropdown
+        conta
+        containerStyle={{
+          width: "300px",
+        }}
+        inputStyle={{
+          height: "55px",
+          borderRadius: "7px",
+          background: "#2C2C2C",
+          autocomplete: "none",
+          outline: "none",
+        }}
+        buttonStyle={{
+          paddingLeft: "6px",
+        }}
+      />
+    </div>
   );
 }
--- src/routes/Phone.tsx
+++ src/routes/Phone.tsx
@@ -1,14 +1,11 @@
 import React from "react";
 import Button from "@mui/material/Button";
 import { useTelegram } from "../hooks/useTelegram";
-import CountrySelect from "../components/Phone/CountrySelect";
 import PN from "../components/Phone/PN";
 
 export default function Phone() {
   const {tg, user} = useTelegram()
-  return <div style={{display:'flex', justifyContent:'space-around',flexDirection:'column',width:'100%',height:'100vh',alignItems:'center'}}>
-    {user.username != undefined ? <h1 style={{fontFamily:'Open Sans'}}>Привет, {user?.username}</h1>:
-    <h1>Привет!</h1>}
+  return <div>
     <PN/>
   </div>
 }
