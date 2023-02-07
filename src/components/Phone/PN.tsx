873fc56b40c33fcf73aea113f3ed33ba0b95ced5
hi-radik <practice.radomir@gmail.com>
2023-02-08 01:57:05 +0300

    ??

--- src/components/Phone/PN.tsx
+++ src/components/Phone/PN.tsx
@@ -5,11 +5,21 @@ import "./bootstrap.css"
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 const ReactPhoneInput = RPI.default ? RPI.default : RPI;
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import logo from '../../assets/logo.svg'
+import { useTelegram } from '../../hooks/useTelegram';
+import MainButton from '../Buttons/MainButton/MainButton';
 
 export default function PN() {
+  const {tg} = useTelegram()
   const [value, setValue]: any = useState();
+  useEffect(()=>{
+    if(value.length){
+      tg.MainButton.show()
+    }else{
+      tg.MainButton.hide()
+    }
+  }, [value])
   return (
     <div className="phone-page">
       <div style={{ width: "100%", display: "flex", justifyContent: "center"}}>
