213d5ab8b6c3746ac24981483ef3e3b9a1665958
hi-radik <practice.radomir@gmail.com>
2023-02-08 21:35:40 +0300

    fix?

--- src/main.tsx
+++ src/main.tsx
@@ -2,18 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Phone from "./routes/Phone";
 
-// const router = createBrowserRouter([
-//   {
-//     path: "/",
-//     element: <App />
-//   },
-// ]);
+const router = createBrowserRouter([
+  {
+    path: "/phone",
+    element: <Phone />
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App/>
-    {/* <RouterProvider router={router} /> */}
+    {/* <App/> */}
+    <RouterProvider router={router} />
   </React.StrictMode>
   
 );
