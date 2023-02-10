acc5f880e27cdad984ccf4e4d2dad4a0b866501c
hi-radik <practice.radomir@gmail.com>
2023-02-10 23:20:50 +0300

    adasd

--- src/routes/ConfirmPhone.tsx
+++ src/routes/ConfirmPhone.tsx
@@ -4,9 +4,11 @@ import { useTelegram } from "../hooks/useTelegram";
 import AnimatedPage from "../components/AnimatedPage";
 import '../components/ConfirmPhone/ConfirmPage.css'
 import { usePhoneCodeStore } from "../stores/smsCodeStore";
+import { useNavigate } from 'react-router-dom';
 export default function ConfirmPhone() {
   const smsCode = usePhoneCodeStore((state) => state.smsCode);
   const { tg } = useTelegram();
+  const navigate = useNavigate()
   useEffect(() => {
     tg.expand();
     tg.MainButton.setParams({
@@ -14,15 +16,16 @@ export default function ConfirmPhone() {
       color: "#FC4C01",
       // color: 'var(--tg-theme-button-color)'
     });
+    tg.MainButton.show();
   }, []);
 
-  useEffect(() => {
-    if (smsCode.join() == '9,9,9,9') {
-      tg.MainButton.show();
-    } else {
-      tg.MainButton.hide();
-    }
-  }, [smsCode]);
+  // useEffect(() => {
+  //   if (smsCode.join() == '9,9,9,9') {
+  //     navigate('/')
+  //   } else {
+  //     tg.alert('Неверный код подтверждения!')
+  //   }
+  // }, [smsCode]);
   return (
     <AnimatedPage>
       <ConfirmInput />
